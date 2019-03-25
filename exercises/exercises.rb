e340295bee4fbae40658c188bc57797634f886d9
Dominic White <your email>
2019-03-25 12:12:34 +0000

    Print names with first letter

---
+++ exercises/exercises.rb
@@ -0,0 +1,46 @@
+def input_students
+  puts "Please enter the names of the students"
+  puts "To finish, just hit return twice"
+  #create an empty array
+  students = []
+  # get the first name
+  name = gets.chomp
+  # while the name is not empty, repeat this code
+  while !name.empty? do
+    #add the student hash to the array
+    students << {name: name, cohort: :november}
+    puts "Now we have #{students.count} students"
+    #get another name from the user
+    name = gets.chomp
+  end
+  # return array of students
+  students
+end
+
+def print_header
+  puts "The students of Villains Academy"
+  puts "-------------"
+end
+
+def print(students)
+  puts "Enter first letter of name"
+  first_letter = gets.chomp
+  # user input for initial
+  students.each_with_index do |student, index|
+    if first_letter.empty? || student[:name][0] == first_letter
+      indexplusone = index + 1
+      puts "#{indexplusone} #{student[:name]} (#{student[:cohort]} cohort)"
+    end
+  end
+end
+
+def print_footer(names)
+  puts "Overall, we have #{names.count} great students"
+end
+
+#nothing happens until we call the methods
+students = input_students
+
+print_header
+print(students)
+print_footer(students)
